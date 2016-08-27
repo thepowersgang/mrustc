924efe07fca8620a8ed9726e473e31b86cff9b2e
John Hodge <tpg@mutabah.net>
2016-08-27 21:14:33 +0800

    Expand - Add std/core to crate root

--- src/expand/mod.cpp
+++ src/expand/mod.cpp
@@ -857,6 +857,7 @@ void Expand(::AST::Crate& crate)
         crate.m_extern_crates.at("std").with_all_macros([&](const auto& name, const auto& mac) {
             crate.m_root_module.add_macro_import( name, mac );
             });
+        crate.m_root_module.add_ext_crate(false, "std", "std", ::AST::MetaItems {});
         break;
     case ::AST::Crate::LOAD_CORE:
         if( crate.m_prelude_path != AST::Path() )
@@ -865,6 +866,7 @@ void Expand(::AST::Crate& crate)
         crate.m_extern_crates.at("core").with_all_macros([&](const auto& name, const auto& mac) {
             crate.m_root_module.add_macro_import( name, mac );
             });
+        crate.m_root_module.add_ext_crate(false, "core", "core", ::AST::MetaItems {});
         break;
     case ::AST::Crate::LOAD_NONE:
         break;
