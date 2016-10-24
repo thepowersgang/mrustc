be1a8b9e0464551a61d9f216c67b0542b33559b0
John Hodge <tpg@mutabah.net>
2016-10-24 11:16:19 +0800

    MIR Ptr - Implement destructor

--- src/mir/mir_ptr.cpp
+++ src/mir/mir_ptr.cpp
@@ -1,9 +1,18 @@
 /*
+ * MRustC - Rust Compiler
+ * - By John Hodge (Mutabah/thePowersGang)
+ *
+ * mir/mir_ptr.cpp
+ * - Destructor for MIR function pointers (cold path code)
  */
 #include "mir_ptr.hpp"
+#include "mir.hpp"
 
 
 ::MIR::FunctionPointer::~FunctionPointer()
 {
+    if( this->ptr ) {
+        delete this->ptr, this->ptr = nullptr;
+    }
 }
 
