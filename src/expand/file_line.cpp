85b8adcc8d9f65e175437699ed3d36362c7ac2b7
John Hodge <tpg@mutabah.net>
2016-10-24 11:16:35 +0800

    Expand - Implement module_path! macro

--- src/expand/file_line.cpp
+++ src/expand/file_line.cpp
@@ -21,6 +21,22 @@ class CExpanderLine:
     }
 };
 
+class CExpanderModulePath:
+    public ExpandProcMacro
+{
+    ::std::unique_ptr<TokenStream> expand(const Span& sp, const AST::Crate& crate, const ::std::string& ident, const TokenTree& tt, AST::Module& mod) override
+    {
+        ::std::string   path_str;
+        for(const auto& comp : mod.path().nodes()) {
+            if( &comp != &mod.path().nodes().front() )
+                path_str += "::";
+            path_str += comp.name();
+        }
+        return box$( TTStreamO(TokenTree( Token(TOK_STRING, mv$(path_str)) )) );
+    }
+};
+
 STATIC_MACRO("file", CExpanderFile);
 STATIC_MACRO("line", CExpanderLine);
+STATIC_MACRO("module_path", CExpanderModulePath);
 
