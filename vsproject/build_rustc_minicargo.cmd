40007460f74b2cda9ab06a22b096fa5b596035f3
John Hodge <tpg@mutabah.net>
2017-09-23 14:53:07 +0800

    VSProject - add a script to build libstd and friends

---
+++ vsproject/build_rustc_minicargo.cmd
@@ -0,0 +1,7 @@
+@echo off
+x64\Release\minicargo.exe ..\rustc-1.19.0-src\src\libstd --script-overrides ..\script-overrides\stable-1.19.0
+if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\rustc-1.19.0-src\src\libpanic_unwind --script-overrides ..\script-overrides\stable-1.19.0
+if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\rustc-1.19.0-src\src\libtest --script-overrides ..\script-overrides\stable-1.19.0
+if %errorlevel% neq 0 exit /b %errorlevel%
