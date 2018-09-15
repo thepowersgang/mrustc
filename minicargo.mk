4a8198baca2cfcee17f83978ab71422a04d53b1a
John Hodge <tpg@ucc.asn.au>
2018-09-15 12:03:28 +0800

    minicargo.mk - Random test target

--- minicargo.mk
+++ minicargo.mk
@@ -114,6 +114,8 @@ $(RUSTCSRC)build/Makefile: $(RUSTCSRC)src/llvm/CMakeLists.txt
 #
 # Developement-only targets
 #
+$(OUTDIR)rustc-build/librustc_trans.hir: $(MRUSTC) LIBS $(LLVM_CONFIG)
+	$(MINICARGO) $(RUSTCSRC)src/librustc_trans --vendor-dir $(RUSTCSRC)src/vendor --output-dir $(dir $@) -L $(OUTDIR) $(MINICARGO_FLAGS)
 $(OUTDIR)rustc-build/librustdoc.hir: $(MRUSTC) LIBS
 	$(MINICARGO) $(RUSTCSRC)src/librustdoc --vendor-dir $(RUSTCSRC)src/vendor --output-dir $(dir $@) -L $(OUTDIR) $(MINICARGO_FLAGS)
 #$(OUTDIR)cargo-build/libserde-1_0_6.hir: $(MRUSTC) LIBS
