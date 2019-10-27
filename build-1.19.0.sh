8c691e5bf130f27fa623213780cdf5eeb1105b4f
John Hodge <tpg@mutabah.net>
2019-10-27 11:01:12 +0800

    scripts - Ensure that 1.19 is downloaded in `build-1.19.0.sh`

--- build-1.19.0.sh
+++ build-1.19.0.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 make
+RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make RUSTCSRC
 RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make test
 RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 time make -f minicargo.mk LIBS
 RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 time make -f minicargo.mk output-1.19.0/stdtest/rustc_data_structures-test_out.txt
