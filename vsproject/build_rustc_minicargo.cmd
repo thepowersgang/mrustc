66353070eba9236219a003023c428cb6f1d1de3e
John Hodge <tpg@mutabah.net>
2019-11-03 15:54:58 +0800

    vsproject scripts - Slight fixes

--- vsproject/build_rustc_minicargo.cmd
+++ vsproject/build_rustc_minicargo.cmd
@@ -1,6 +1,6 @@
 @call build_std.cmd
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@mkdir output\rustc-build
+@mkdir %OUTDIR%\rustc-build
 x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\rustc -L %OUTDIR% --output-dir %OUTDIR%\rustc-build --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor
 @if %errorlevel% neq 0 exit /b %errorlevel%
--- vsproject/run_tests.cmd
+++ vsproject/run_tests.cmd
@@ -1,6 +1,5 @@
-@echo off
-call build_std.cmd
-if %errorlevel% neq 0 exit /b %errorlevel%
+@call build_std.cmd
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-mkdir output\tests\
-x64\Release\testrunner.exe ..\rustc-1.19.0-src\src\test\run-pass -o output\tests\
+@mkdir %OUTDIR%\tests\
+x64\Release\testrunner.exe ..\rustc-%RUSTC_VERSION%-src\src\test\run-pass -o %OUTDIR%\tests\ -L %OUTDIR%
