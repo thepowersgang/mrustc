9addf62c2793fad9cf6807f106fd4f5bf13806af
John Hodge <tpg@mutabah.net>
2019-11-09 21:03:24 +0800

    vsproject - Tweak scripts to run smiri

---
+++ vsproject/build_hello_mmir.cmd
@@ -0,0 +1,16 @@
+@echo off
+@set RUSTC_VERSION=1.29.0
+@set OUTDIR=output-%RUSTC_VERSION%-mmir
+@mkdir %OUTDIR%
+
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libstd --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows --output-dir %OUTDIR% -Z emit-mmir
+if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libpanic_unwind --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows --output-dir %OUTDIR% -Z emit-mmir
+if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libtest --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows --output-dir %OUTDIR% -Z emit-mmir
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+x64\Release\mrustc.exe ..\rustc-%RUSTC_VERSION%-src\src\test\run-pass\hello.rs -L %OUTDIR% -o %OUTDIR%\hello.exe -C codegen-type=monomir
+if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\standalone_miri.exe %OUTDIR%\hello.exe.mir
+if %errorlevel% neq 0 exit /b %errorlevel%
