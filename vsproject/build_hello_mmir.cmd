5fd9a373c6aa8034ec40b38986bafda8ea6344e2
John Hodge <tpg@mutabah.net>
2019-12-18 14:54:00 +0800

    vsproject - Tweaks to builder scripts

--- vsproject/build_hello_mmir.cmd
+++ vsproject/build_hello_mmir.cmd
@@ -10,7 +10,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libtest --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows --output-dir %OUTDIR% -Z emit-mmir
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-x64\Release\mrustc.exe ..\rustc-%RUSTC_VERSION%-src\src\test\run-pass\hello.rs -L %OUTDIR% -o %OUTDIR%\hello.exe -C codegen-type=monomir
+x64\Release\mrustc.exe ..\rustc-%RUSTC_VERSION%-src\src\test\run-pass\hello.rs -L %OUTDIR% -o %OUTDIR%\hello.exe -C codegen-type=monomir > %OUTDIR%\hello.exe_dbg.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
 x64\Release\standalone_miri.exe --logfile smiri.log %OUTDIR%\hello.exe.mir
 if %errorlevel% neq 0 exit /b %errorlevel%
--- vsproject/build_std.cmd
+++ vsproject/build_std.cmd
@@ -1,11 +1,14 @@
 @set RUSTC_VERSION=1.29.0
 @set OUTDIR=output-%RUSTC_VERSION%
+@set STD_ARGS=--output-dir %OUTDIR%
+@set STD_ARGS=%STD_ARGS% --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor
+@set STD_ARGS=%STD_ARGS% --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows
 @mkdir %OUTDIR%
-x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libstd --output-dir %OUTDIR% --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libstd %STD_ARGS%
 @if %errorlevel% neq 0 exit /b %errorlevel%
-x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libpanic_unwind --output-dir %OUTDIR% --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libpanic_unwind %STD_ARGS%
 @if %errorlevel% neq 0 exit /b %errorlevel%
-x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libtest --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor --output-dir %OUTDIR% --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libtest %STD_ARGS%
 @if %errorlevel% neq 0 exit /b %errorlevel%
 x64\Release\minicargo.exe ..\lib\libproc_macro --output-dir %OUTDIR%
 @if %errorlevel% neq 0 exit /b %errorlevel%
