0a4dbfb868eb9956cec2372a74fb679c772d8c89
John Hodge <tpg@mutabah.net>
2020-05-17 11:49:53 +0800

    vsproject - Test script to ensure that 1.19 std+hello builds

---
+++ vsproject/run_hello_119.cmd
@@ -0,0 +1,21 @@
+@set RUSTC_VERSION=1.19.0
+@set MRUSTC_TARGET_VER=1.19
+@set OUTDIR=output-%RUSTC_VERSION%
+@set STD_ARGS=--output-dir %OUTDIR%
+@set STD_ARGS=%STD_ARGS% --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor
+@set STD_ARGS=%STD_ARGS% --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows
+@set STD_ARGS=%STD_ARGS% --features backtrace
+@mkdir %OUTDIR%
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libstd %STD_ARGS%
+@if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libpanic_unwind %STD_ARGS%
+@if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libtest %STD_ARGS%
+@if %errorlevel% neq 0 exit /b %errorlevel%
+x64\Release\minicargo.exe ..\lib\libproc_macro --output-dir %OUTDIR%
+@if %errorlevel% neq 0 exit /b %errorlevel%
+
+x64\Release\mrustc.exe ..\rustc-%RUSTC_VERSION%-src\src\test\run-pass\hello.rs -L %OUTDIR% -o %OUTDIR%\hello.exe -g
+@if %errorlevel% neq 0 exit /b %errorlevel%
+%OUTDIR%\hello.exe
+@if %errorlevel% neq 0 exit /b %errorlevel%
