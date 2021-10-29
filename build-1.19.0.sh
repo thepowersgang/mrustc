953b8a9e9c129e22338daf1a837852c9d20afc06
John Hodge <tpg@ucc.asn.au>
2021-10-29 18:11:42 +0800

    root - Remove `time` calls from build scripts, add 1.54 build script

--- build-1.19.0.sh
+++ build-1.19.0.sh
@@ -3,7 +3,7 @@ set -e
 make
 RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make RUSTCSRC
 RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make test
-RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 time make -f minicargo.mk LIBS
-RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 time make -f minicargo.mk output-1.19.0/stdtest/rustc_data_structures-test_out.txt
-RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 time make -f minicargo.mk output-1.19.0/rustc
-RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 time make -f minicargo.mk output-1.19.0/cargo
+RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make -f minicargo.mk LIBS
+RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make -f minicargo.mk output-1.19.0/stdtest/rustc_data_structures-test_out.txt
+RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make -f minicargo.mk output-1.19.0/rustc
+RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0 make -f minicargo.mk output-1.19.0/cargo
--- test_smiri.sh
+++ test_smiri.sh
@@ -5,6 +5,6 @@ make all
 make -C tools/standalone_miri || exit 1
 make -f minicargo.mk MMIR=1 LIBS V= || exit 1
 echo "--- mrustc -o output-mmir/hello"
-time ./bin/mrustc rustc-1.19.0-src/src/test/run-pass/hello.rs -O -C codegen-type=monomir -o output-mmir/hello -L output-mmir/ > output-mmir/hello_dbg.txt || exit 1
+./bin/mrustc rustc-1.19.0-src/src/test/run-pass/hello.rs -O -C codegen-type=monomir -o output-mmir/hello -L output-mmir/ > output-mmir/hello_dbg.txt || exit 1
 echo "--- standalone_miri output-mmir/hello.mir"
-time ./bin/standalone_miri output-mmir/hello.mir --logfile smiri_hello.log
+./bin/standalone_miri output-mmir/hello.mir --logfile smiri_hello.log
