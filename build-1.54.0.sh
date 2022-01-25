860e2674d827820d6a71f6f4d594658ec11c2670
John Hodge <tpg@ucc.asn.au>
2022-01-25 18:35:45 +0800

    1.54 - Tweak overrides and patches to disable use of `global_asm` and SSE intrinsics

--- build-1.54.0.sh
+++ build-1.54.0.sh
@@ -5,8 +5,8 @@ RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make RUSTCSRC
 RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make -f minicargo.mk LIBS
 RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make test
 RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make local_tests
-# Build just rustc-driver BEFORE building llvm
-RUSTC_INSTALL_BINDIR=bin RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make -f minicargo.mk output-1.54.0/rustc-build/librustc_driver.rlib
+## Build just rustc-driver BEFORE building llvm
+#RUSTC_INSTALL_BINDIR=bin RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make -f minicargo.mk output-1.54.0/rustc-build/librustc_driver.rlib
 RUSTC_INSTALL_BINDIR=bin RUSTC_VERSION=1.54.0 MRUSTC_TARGET_VER=1.54 OUTDIR_SUF=-1.54.0 make -f minicargo.mk output-1.54.0/rustc
 ./output-1.54.0/rustc --version
 
