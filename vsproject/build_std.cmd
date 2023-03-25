f1e0846d13d07958ce20a0390a3dc8cb29dda43d
John Hodge <tpg@mutabah.net>
2023-03-25 12:19:18 +0800

    rustc patch 1.29 - Fix so it can be applied by `minipatch`, add a fix for bad annotations in `librustc_mir`

--- vsproject/build_std.cmd
+++ vsproject/build_std.cmd
@@ -7,6 +7,10 @@
 @set STD_ARGS=%STD_ARGS% --output-dir %OUTDIR%
 @set STD_ARGS=%STD_ARGS% --script-overrides ..\script-overrides\stable-%RUSTC_VERSION%-windows
 @mkdir %OUTDIR%
+
+x64\Release\minipatch.exe ..\rustc-1.29.0-src.patch ..\rustc-1.29.0-src
+@if %errorlevel% neq 0 exit /b %errorlevel%
+
 x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libstd %STD_ARGS%
 @if %errorlevel% neq 0 exit /b %errorlevel%
 x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\libpanic_unwind %STD_ARGS%
