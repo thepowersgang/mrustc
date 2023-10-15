b3726e23c5436934c8a795b5d63620a9fb746372
John Hodge <tpg@mutabah.net>
2023-10-15 12:13:38 +0800

    (minor) Add `MIR_BorrowCheck` to MIR operations header

--- src/mir/operations.hpp
+++ src/mir/operations.hpp
@@ -18,4 +18,6 @@ extern void MIR_Cleanup(const StaticTraitResolve& resolve, const ::HIR::ItemPath
 extern void MIR_Optimise(const StaticTraitResolve& resolve, const ::HIR::ItemPath& path, ::MIR::Function& fcn, const ::HIR::Function::args_t& args, const ::HIR::TypeRef& ret_type, bool do_inline=true);
 extern void MIR_SortBlocks(const StaticTraitResolve& resolve, const ::HIR::ItemPath& path, ::MIR::Function& fcn);
 
+extern void MIR_BorrowCheck(const StaticTraitResolve& resolve, const ::HIR::ItemPath& path, ::MIR::Function& fcn, const ::HIR::Function::args_t& args, const ::HIR::TypeRef& ret_type);
+
 extern void MIR_Dump_Fcn(::std::ostream& sink, const ::MIR::Function& fcn, unsigned int il=0);
