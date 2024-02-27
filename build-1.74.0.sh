a8b3ee5469c403910a8567e1f491c2b810dd786f
John Hodge <tpg@mutabah.net>
2024-02-27 23:13:47 +0800

    minicargo.mk - 1.74 rustc is being built, only 20% complete

--- build-1.74.0.sh
+++ build-1.74.0.sh
@@ -7,7 +7,7 @@ make -f minicargo.mk LIBS $@
 make test $@
 make local_tests $@
 ## Build just rustc-driver BEFORE building llvm
-#RUSTC_INSTALL_BINDIR=bin make -f minicargo.mk output-1.74.0/rustc-build/librustc_driver.rlib
+RUSTC_INSTALL_BINDIR=bin make -f minicargo.mk output-1.74.0/rustc-build/librustc_driver.rlib
 #RUSTC_INSTALL_BINDIR=bin make -f minicargo.mk output-1.74.0/rustc $@
 #./output-1.74.0/rustc --version
 
