1a94f9855bfe218361f1abda7596408c497d7c3c
John Hodge <tpg@mutabah.net>
2024-11-01 22:47:06 +0800

    scripts - Re-add rustc to 1.74 build script

--- build-1.74.0.sh
+++ build-1.74.0.sh
@@ -7,8 +7,8 @@ make -f minicargo.mk LIBS $@
 make test $@
 make local_tests $@
 
-LIBGIT2_SYS_USE_PKG_CONFIG=1 make -f minicargo.mk -j ${PARLEVEL:-1} output-1.74.0/cargo $@
-./output-1.74.0/cargo --version
-
 RUSTC_INSTALL_BINDIR=bin make -f minicargo.mk output-1.74.0/rustc $@
 ./output-1.74.0/rustc --version
+
+LIBGIT2_SYS_USE_PKG_CONFIG=1 make -f minicargo.mk -j ${PARLEVEL:-1} output-1.74.0/cargo $@
+./output-1.74.0/cargo --version
